d762d2ce24513f9a68f308e9d6c420b3bb6702e8
minafadi <71638501+minafadi@users.noreply.github.com>
2022-05-04 14:06:23 +0200

    Completed the CompanyAdmin Class

---
+++ src/CompanyAdmin.java
@@ -0,0 +1,12 @@
+public class CompanyAdmin {
+    private Company companies[];
+
+
+    public void addCompany(String name) {
+        //incomplete
+    }
+
+    public void updateCompanyDescription(Company chosenCompany, String newDescription) {
+        chosenCompany.setCompanyDescription(newDescription);
+    }
+}
