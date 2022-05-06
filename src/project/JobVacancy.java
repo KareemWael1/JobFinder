9180bf6b916d791c4005e83eba3dff28e261d7d2
aliragy <ali.khaled.ragy@gmail.com>
2022-05-06 22:02:52 +0200

    fdg

---
+++ src/project/JobVacancy.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ali
+ */
+
+import java.util.ArrayList;
+
+public class JobVacancy {
+    
+    
+   int COMPANY_ID;
+    String jobtitle;
+    String jobdescription;
+    
+    public ArrayList<jobapplication>   applications()= new ArrayList<>();
+    
+   
+    public Arraylist<jobapplication> get jobapplications()
+    {
+    return applications;
+    }
+    
+    
+    
+    public void addapplication (jobApplication application){
+        
+        
+    }
+    
+    public String getjobdescription(){
+    
+    return jobdescription;
+    
+    }
+    
+    
+    public void updateapplication (String info){
+        
+        info.setinfo(newinfo);
+    
+    
+    }
+    
+    
+   String getjobtitle(){
+        
+        return jobtitle;
+    }
+            
+            
+   private getAllApplications()
+   
+   {
+       for (int i=0;i<jobapplications.size();i++)
+           
+           return jobapplications.get(i).getname();
+           
+   }
+    
+    void removeapplication(int jobapplicationidx)
+    {
+    jobapplications.remove(jobapplicationidx);
+    
+    }
+    
+    
+    
+    
+}
---
+++ src/project/Jobposter.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ali
+ */
+
+import java.util.ArrayList;
+
+
+public class Jobposter extends person {
+    
+    int COMPANY_id;
+    
+  private ArrayList<jobvacancy> jobvacancies= new ArrayList <jobvacancy>();
+          
+  
+     private Arraylist<jobvacancy> get jobvacancies()
+    {
+    return applications;
+    }
+          
+          
+          addjobvacancy(string title)
+   
+   final static private ArrayList<jobvacancy>  jobvacancies()=new ArrayList<>();
+   
+   
+   public ArrayList<jobvacancy> getjobvacancies(){
+   
+   return this.jobvacancies;
+   
+   public static ArrayList<jobvacancies> getjobvacancies(){
+           
+           return jobvacancies;
+   
+   }
+   
+   
+   public void addjobvacancy(string info,string title){
+           
+           vacancies.add(String title, String info)
+                   }
+    
+}
+
+void setApplicationstatus(bool status){
+
+        this.status= status;
+}
+
+
+public void deletejob(int jobvacancyidx){
+
+jobvacancies.remove (jobvacancyidx)
+}
+
+
+}
