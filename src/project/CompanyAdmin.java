1b2eed1f18c0efc70aa942386dce7044abe76576
minafadi <71638501+minafadi@users.noreply.github.com>
2022-05-10 09:57:45 +0200

    Added addJobPoster() method

--- src/project/CompanyAdmin.java
+++ src/project/CompanyAdmin.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class CompanyAdmin extends Person {
     final static private ArrayList<Company> companies = new ArrayList<>(); // final so companies do not point to other list
+    final static private ArrayList<JobPoster> jobPosters = new ArrayList<>();
 
     CompanyAdmin(String name, String email, String gender, String password){
         super(name, email, gender, password);
@@ -12,9 +13,17 @@ public class CompanyAdmin extends Person {
         return companies;
     }
 
+    public static ArrayList<JobPoster> getJobPosters() {
+        return jobPosters;
+    }
+
     public static void addCompany(Company name) {
         name.setID(companies.size());
         companies.add(name);
+    }
+
+    public static void addJobPoster(JobPoster name) {
+        jobPosters.add(name);
     }
 
     public static void updateCompanyDescription(Company chosenCompany, String newDescription) {
